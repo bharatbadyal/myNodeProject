ae35a6b7e98d09021f3884b93ba2dfd6e69eac69
Bharat <myprof219@gmail.com>
2025-06-07 17:53:47 +0530

    Have done loops logical problem solving

--- loops_prob.js
+++ loops_prob.js
@@ -22,16 +22,44 @@
 
 // 02. Write a function that will return the number of -ve no in an array
 
-function countNegNo(arr){
-    let totalNum = 0;
-    for(let i =0; i < arr.length; i++){
-        if(arr[i] < 0){
-            totalNum++;
+// function countNegNo(arr){
+//     let totalNum = 0;
+//     for(let i =0; i < arr.length; i++){
+//         if(arr[i] < 0){
+//             totalNum++;
+//         }
+//     }
+//     return totalNum;
+// }
+
+// let arr1 = [-2,3,-4,5,-3,-6]
+// let ans = countNegNo(arr1)
+// console.log(ans)
+
+// 03. Write a function that will return the largest no in given array.
+
+function lasrgestNo(arr){
+    let largest = -Infinity
+    for(let i =0 ; i<arr.length; i++){
+        if(arr[i] > largest){
+            largest = arr[i];
         }
     }
-    return totalNum;
+    return largest;
 }
 
-let arr1 = [-2,3,-4,5,-3,-6]
-let ans = countNegNo(arr1)
-console.log(ans)
+let arr2 = [2,4,6,1,113,-13,15,17]
+console.log("The largest no in array is : ",lasrgestNo(arr2))
+
+// 04. Wirte a function that will return smallest no in given array.
+let smallest = Infinity;
+function smallestNo(arr){
+    for( let i = 0; i<arr.length; i++){
+        if(arr[i]< smallest){
+            smallest = arr[i];
+        }
+    }
+    return smallest;
+}
+
+console.log('The smallest no in array is: ', smallestNo(arr2))
