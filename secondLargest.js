01efe4f537fb23d431bd25bbb1da04c061e1771d
Bharat <myprof219@gmail.com>
2025-06-08 20:15:15 +0530

    Loop in Loop

--- secondLargest.js
+++ secondLargest.js
@@ -1,19 +1,32 @@
-// Write a function to find the second largest no in array
-let largest = -Infinity;
-let secondLargest = -Infinity
+// // Write a function to find the second largest no in array
+// let largest = -Infinity;
+// let secondLargest = -Infinity
 
 
-function secondLargestNo(arr){
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] > largest){
-            secondLargest = largest;
-            largest = arr[i];
-        }else if(arr[i]>secondLargest){
-            secondLargest = arr[i];
+// function secondLargestNo(arr){
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] > largest){
+//             secondLargest = largest;
+//             largest = arr[i];
+//         }else if(arr[i]>secondLargest){
+//             secondLargest = arr[i];
+//         }
+//     }
+//     return secondLargest;
+// }
+
+// let arr = [4,9,0,2,8,7,1];
+// console.log("The second Largest No. is: ",secondLargestNo(arr))
+
+
+// Lets Practice Loop in Loop
+
+for(let i=0; i<1; i++){
+    for(let j=0; j<2; j++){
+        for(let k=0; k<3;k++){
+            
+        console.log("The value of i for j for k is: ",i,j,k)
+
         }
-    }
-    return secondLargest;
+        }
 }
-
-let arr = [4,9,0,2,8,7,1];
-console.log("The second Largest No. is: ",secondLargestNo(arr))
