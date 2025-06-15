0be54c0c86e1ad748202c4000629a0e10aae2454
Bharat <myprof219@gmail.com>
2025-06-15 10:39:17 +0530

    started practicing arrays after completing the theory for last two days

---
+++ arrays/removeDuplicate.js
@@ -0,0 +1,14 @@
+let arr = [0,1,1,1,2,2,3,3,4];
+
+function removeDuplicate(arr){
+    let x=0;
+    for(let i=0; i<arr.length;i++){
+        if(arr[i]>arr[x]){
+            x++;
+            arr[x]=arr[i];
+        }
+    }
+    return x+1;
+}
+
+console.log(removeDuplicate(arr))
